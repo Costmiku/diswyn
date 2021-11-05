3a9107143f1c30b955d98a026d568db403f3f6d1
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-05 20:31:49 +0100

    eval cmd

--- main.py
+++ main.py
@@ -58,4 +58,17 @@ async def globalprefix(ctx, prefix, permanent = ''):
     else:
         await ctx.send('You are not my owner!')
 
+# eval command
+@bot.command(aliases=['eval'])
+async def boteval(ctx, *, code):
+    if (ctx.message.author.id == bot.inf.owner.id) or (ctx.message.author.id in ENV['ADDITIONAL_OWN_PERMS']):
+        try:
+            result = eval(code)
+            if result:
+                await ctx.send(f'```py\n{result}\n```')
+        except Exception as e:
+            await ctx.send(f'```py\n{e}\n```')
+    else:
+        await ctx.send('You are not my owner!')
+
 bot.run(ENV['DISCORD_TOKEN'])
