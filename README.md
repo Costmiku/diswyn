ed4230507e1f0615b73f9f74e206fc6c590aeded
Mizu <40831463+Rexxt@users.noreply.github.com>
2021-11-05 18:19:14 +0100

    Bot update

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # diswyn
- 
+A general-purpose, self-hostable Discord bot.
+
+## Configuration for self-host
+### env.json
+In the bot folder, create an env.json containing this:
+```json
+{
+    "DISCORD_TOKEN": "bot token here",
+    "ADDITIONAL_OWN_PERMS": []
+}
+```
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import json
 from discord.ext import commands
 
 ENV = json.load(open('env.json'))
+data = json.load(open('data.json'))
 
-bot = commands.Bot(command_prefix='!!')
+bot = commands.Bot(command_prefix=data['prefix'])
 
 @bot.listen('on_ready')
 async def on_ready():
@@ -13,9 +14,48 @@ async def on_ready():
     print('| ID:', bot.user.id)
     print('| Prefix:', f'"{bot.command_prefix}"')
     print('------')
+    bot.inf = await bot.application_info()
+    bot.owner = f'{bot.inf.owner.name}#{bot.inf.owner.discriminator}'
 
+# log command usage
+@bot.listen('on_command')
+async def on_command(ctx, *error):
+    print(f'{ctx.message.author} (ID {ctx.message.author.id}) used command "{ctx.message.content}"')
+    if error: print(f'Error: {error}')
+
+# show the bot's ping
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(bot.latency*1000)}ms')
+
+# echo command
+@bot.command()
+async def echo(ctx, *, msg):
+    await ctx.message.delete()
+    await ctx.send(msg)
+
+# stats command
+@bot.command()
+async def stats(ctx):
+    # neat embed
+    embed = discord.Embed(title='Statistics', description='', color=0x00ff00)
+    embed.add_field(name='Prefix', value=f'"{bot.command_prefix}"')
+    embed.add_field(name='Owner', value=f'{bot.owner}')
+    embed.add_field(name='Guilds', value=f'{len(bot.guilds)}')
+    embed.add_field(name='Users', value=f'{len(bot.users)}')
+    embed.add_field(name='Latency', value=f'{round(bot.latency*1000)}ms')
+    await ctx.send(embed=embed)
+
+# command to change the bot's global prefix
+@bot.command()
+async def globalprefix(ctx, prefix, permanent = ''):
+    if (ctx.message.author.id == bot.inf.owner.id) or (ctx.message.author.id in ENV['ADDITIONAL_OWN_PERMS']):
+        if permanent == 'perm' or permanent == 'permanent':
+            data['prefix'] = prefix
+            json.dump(data, open('data.json', 'w'), indent=4)
+        bot.command_prefix = prefix
+        await ctx.send(f'Prefix changed to "{prefix}"')
+    else:
+        await ctx.send('You are not my owner!')
 
 bot.run(ENV['DISCORD_TOKEN'])
